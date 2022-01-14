7b2d8991448cc1b06afd1eb6250a76a68adeaafb
Bárbara Romero <bqromero@gmail.com>
2022-01-14 13:53:32 -0300

    Alterar configurações do body

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import react, {Component} from "react";
 import "./App.css";
 import LandingPage from "./componentes/pages/LandingPage/landinpage";
-import Login from "./componentes/pages/LandingPage/landinpage";
-
 
 export default class App extends Component {
     render(){
