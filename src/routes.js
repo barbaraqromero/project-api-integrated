8f8e71ba03fda183d748c249337a6c40bb7daa8e
Bárbara Romero <bqromero@gmail.com>
2022-01-19 07:07:18 -0300

    Criar modelo de rotas

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./auth";
+import Login from "./components/pages/Login";
+import Dashboard from "./components/pages/Dashboard";
+import Landing from "./components/pages/LandingPage";
+
+const Routes = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <PrivateRoute path="/dashboard" component={Dashboard} />
+      <Route exact path="/landing" component={LandingPage} />
+    </Switch>
+  </Router>
+);
+
+export default Routes;
